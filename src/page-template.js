d6073353fa33ad853d44695817f19c440f8bb55c
cat - lin <plummorgan@gmail.com>
2020-10-11 19:35:07 -0700

    added bootstrap and css styles

--- src/page-template.js
+++ src/page-template.js
@@ -3,11 +3,11 @@ const generateManager = (manager) => {
     <div class="card border-dark mb-3" style="max-width: 18rem;">
         <div class="card-header">${manager.getName()}</div>
         <div class="card-body text-dark">
-            <h5 class="card-title">${manager.getRole()}</h5>
-            <ul class="card-text">
-                <il>ID: ${manager.getId()}</il>
-                <il>Email: ${manager.getEmail()}</il>
-                <il>Office Number: ${manager.getOfficeNumber()}</il>
+            <h5 class="card-title"><i class="fab fa-fort-awesome-alt"></i>${manager.getRole()}</h5>
+            <ul class="list-group">
+                <il class="list-group-item">ID: ${manager.getId()}</il>
+                <il class="list-group-item">Email: ${manager.getEmail()}</il>
+                <il class="list-group-item">Office Number: ${manager.getOfficeNumber()}</il>
             </ul>
         </div>
     </div>
@@ -19,11 +19,11 @@ const generateEngineer = (engineer) => {
     <div class="card border-dark mb-3" style="max-width: 18rem;">
         <div class="card-header">${engineer.getName()}</div>
         <div class="card-body text-dark">
-            <h5 class="card-title">${engineer.getRole()}</h5>
-            <ul class="card-text">
-                <il>ID: ${engineer.getId()}</il>
-                <il>Email: ${engineer.getEmail()}</il>
-                <il>Github: ${engineer.getGithub()}</il>
+            <h5 class="card-title"><i class="fas fa-file-code"></i>${engineer.getRole()}</h5>
+            <ul class="list-group">
+                <il class="list-group-item">ID: ${engineer.getId()}</il>
+                <il class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getName}'s Email</a></il>
+                <il class="list-group-item">Github: <a href="https://github.com/${engineer.getGithub()}">${engineer.getName}'s Github</a></il>
             </ul>
         </div>
     </div>
@@ -35,11 +35,11 @@ const generateIntern = (intern) => {
     <div class="card border-dark mb-3" style="max-width: 18rem;">
         <div class="card-header">${intern.getName()}</div>
         <div class="card-body text-dark">
-            <h5 class="card-title">${intern.getRole()}</h5>
-            <ul class="card-text">
-                <il>ID: ${intern.getId()}</il>
-                <il>Email: ${intern.getEmail()}</il>
-                <il>School: ${intern.getSchool()}</il>
+            <h5 class="card-title"><i class="fas fa-copy"></i>${intern.getRole()}</h5>
+            <ul class="list-group">
+                <il class="list-group-item">ID: ${intern.getId()}</il>
+                <il class="list-group-item">Email: ${intern.getEmail()}</il>
+                <il class="list-group-item">School: ${intern.getSchool()}</il>
             </ul>
         </div>
     </div>
@@ -66,6 +66,7 @@ const generatePage = (team) => {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Team Profiles</title>
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+            <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
             <link rel="stylesheet" href="./style.css">
         </head>
         <body>
